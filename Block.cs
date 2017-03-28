61cb2a2f69c1177cadf37b9b17809eb08f1e0efc
DAizer <ragemiel117@gmail.com>
2017-03-28 09:55:19 +0200

    Delete Block.cs

--- Block.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Block : MonoBehaviour {
-	void OnCollisionEnter2D(Collision2D other){
-		Destroy (gameObject);
-	}
-}
